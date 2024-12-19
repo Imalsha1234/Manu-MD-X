cb54aca61f06e6976abd745f01b798619e313826
Imalsha1234 <imalshadilshara90@gmail.com>
2024-12-19 17:45:05 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Manu-MD&rQVClShL#QW6KNb-Ve23IDpZh6NtImKelpE8H0iPJ-LspF2nJ7fY",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&edsiiBJQ#ZjkxV8eeInnRCSuk-yy4R6nJAewO6cDoNMyCPlCS8ZA",
 };
